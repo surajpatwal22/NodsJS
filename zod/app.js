3988f58a49d18334540add9a799d863a461bccf4
surajpatwal22 <patwal0suraj@gmail.com>
2025-02-03 22:22:01 +0530

    zod

---
+++ zod/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
