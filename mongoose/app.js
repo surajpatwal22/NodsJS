3925f09fdc4ed306a350318c1870d159bcdefe60
surajpatwal22 <patwal0suraj@gmail.com>
2025-03-27 23:35:37 +0530

    data

--- mongoose/app.js
+++ mongoose/app.js
@@ -15,13 +15,13 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   User.findById('5baa2528563f16379fc8a610')
     .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+        req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
@@ -34,7 +34,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/test?retryWrites=true'
+    'mongodb+srv://surajPatwal:eNesP1nECh8a9h3r@cluster0.sod7s0g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
   )
   .then(result => {
     app.listen(3000);
